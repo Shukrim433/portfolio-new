91b6d3378b2a81751696094a36962aba615f55de
Shukri <150862418+Shukrim433@users.noreply.github.com>
2025-04-26 15:59:53 +0100

    array of work / education experience for timeline component

---
+++ src/assets/timelineElements.js
@@ -0,0 +1,53 @@
+let timelineElements = [
+  {
+    id: 1,
+    title: "Volunteer Junior Developer",
+    institution: "Thomas King",
+    description: "Volunteer Junior Developer at a Software Development business. ",
+    date: "2025- current",
+    type: "work"
+  },
+  {
+    id: 2,
+    title: "Full Stack Web Development Bootcamp",
+    institution: "University of Birmingham",
+    description: "bla bla bla",
+    date: "2024",
+    type: "school"
+  },
+  {
+    id: 3,
+    title: "Stock Processor",
+    institution: "Cancer Research UK",
+    description: "bla bla bla",
+    date: "2023",
+    type: "work"
+  },
+  {
+    id: 4,
+    title: "Temporary Sales Assistant",
+    institution: "H&M",
+    description: "bla bla bla",
+    date: "2022",
+    type: "work"
+  },
+  {
+    id: 4,
+    title: "A Levels",
+    institution: "Broadway Academy",
+    description: "bla bla bla",
+    date: "2022",
+    type: "school"
+  },
+  {
+    id: 5,
+    title: "GCSEs",
+    institution: "Birchfield Independant Girls School",
+    description: "bla bla bla",
+    date: "2020",
+    type: "school"
+  },
+];
+
+
+export default timelineElements;
