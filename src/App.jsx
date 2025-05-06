44cf967ce955e5f04faf73a39d51f5c4abd211d9
Shukri <150862418+Shukrim433@users.noreply.github.com>
2025-05-06 15:54:01 +0100

    added react-hot-toast Toaster component to the top of the application

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Outlet } from "react-router-dom";
 import "./index.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <div className="App flex flex-col min-h-[100vh] bg-beige">
       <Navbar />
       <div className="body">
+        <Toaster />
         <Outlet />
       </div>
       <Footer />
